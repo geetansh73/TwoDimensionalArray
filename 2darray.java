b448d3f65f4a3ad0a23f1b2d446e4272589fb5c8
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-07-25 19:57:00 +0530

    Create 2darray.java

---
+++ 2darray.java
@@ -0,0 +1,15 @@
+class twodarray
+{
+public static void main(String args[])
+{
+int arr[][]={{1,2,3},{1,2,3}};
+int i,j;
+for(i=0;i<2;i++)
+{
+for(j=0;j<3;j++)
+{
+System.out.print(arr[i][j]);
+}
+}
+}
+}
